837d788c176f95872facd1c85ca50aeca3b5c33b
Fl4tk <symphony0820@gmail.com>
2022-05-04 13:23:47 +0900

    SECRET_KEY changed

--- vegempproject/vegempproject/settings.py
+++ vegempproject/vegempproject/settings.py
@@ -9,7 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iyz(%(yrf38+-2!pq7k5xs9p!dqs%cvt(#6&!+urkvb+wf1*wi'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
